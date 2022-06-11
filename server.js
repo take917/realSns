54f5f695afeacf919d47fb80f0a2dd10293bca28
take917 <1tktk2tk3@gmail.com>
2022-06-11 13:20:26 +0900

    ✨ make pages

--- server.js
+++ server.js
@@ -1,6 +1,14 @@
 const express = require("express");
 const app = express();
+const userRouter = require("./routes/user");
+const authRouter = require("./routes/auth");
+const postsRouter = require("./routes/posts");
+
 const PORT = 3000;
+
+app.use("/api/users", userRouter);
+app.use("/api/auth", authRouter);
+app.use("/api/posts", postsRouter);
 
 app.get("/", (req, res) => {
   res.send("hello express");
