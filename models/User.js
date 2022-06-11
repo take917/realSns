a6349469051ed7be980cff3a90031d336db9ebb0
take917 <1tktk2tk3@gmail.com>
2022-06-11 14:17:27 +0900

    ⚡️Send message from postman to mongoose

--- models/User.js
+++ models/User.js
@@ -48,3 +48,4 @@ const UserSchema = new mongoose.Schema(
   },
   { timestamps: true }
 );
+module.exports = mongoose.model("User", UserSchema);
--- server.js
+++ server.js
@@ -5,7 +5,17 @@ const authRouter = require("./routes/auth");
 const postsRouter = require("./routes/posts");
 
 const PORT = 3000;
-
+const mongoose = require("mongoose");
+require("dotenv").config();
+mongoose
+  .connect(process.env.MONGOURL)
+  .then(() => {
+    console.log("DBと接続中....");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+app.use(express.json());
 app.use("/api/users", userRouter);
 app.use("/api/auth", authRouter);
 app.use("/api/posts", postsRouter);
